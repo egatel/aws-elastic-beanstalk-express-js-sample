7210c010346060f43db706c99538602302773136
Sylwester Telega <sylwestertelega@Sylwesters-MacBook-Pro.local>
2022-11-28 21:02:55 +0100

    Staram sie zmieniac swoj kraj

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! - the Cat is trying to change the World too.'));
+app.get('/', (req, res) => res.send('Hello World! - the Cat is trying to change the Poland  too.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
